dfd6a33f7b5d45a26429f2965e9f449836988137
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-08-26 20:52:30 -0300

    realizado o exercico 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,6 +3,9 @@ const data = require('../data/zoo_data');
 const { employees } = data;
 
 function getEmployeeByName(employeeName) {
+  if (employeeName === undefined) {
+    return {};
+  }
   if (employeeName !== ('')) {
     return employees.map((element) => element)
       .find((element) => element.firstName === employeeName || element.lastName === employeeName);
