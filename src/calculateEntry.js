cf093628df2351690d0dd4e3a74e9e66bf54639b
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-08-28 01:02:55 -0300

    corrigindo lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -16,30 +16,21 @@ function countEntrants(entrants) {
   return { adult, child, senior };
 }
 
+const calculate = (adult, child, senior) => {
+  const valorAdult = adult * 49.99;
+  const valorChild = child * 20.99;
+  const valorSenior = senior * 24.99;
+  return valorAdult + valorChild + valorSenior;
+};
+
 function calculateEntry(entrants = 0) {
-  if (Object.keys(entrants).length === 0) {
-    return 0;
-  }
   if (entrants.length >= 1) {
-    let adult = 0;
-    let child = 0;
-    let senior = 0;
-    entrants.forEach((element) => {
-      if (element.age >= 50) {
-        senior += 1;
-      } else if (element.age >= 18) {
-        adult += 1;
-      } else {
-        child += 1;
-      }
-    });
-    const valorAdult = adult * 49.99;
-    const valorChild = child * 20.99;
-    const valorSenior = senior * 24.99;
-    const total = valorAdult + valorChild + valorSenior;
-    return total;
-  }
-  return entrants;
+    const custumers = countEntrants(entrants);
+    const { adult } = custumers;
+    const { child } = custumers;
+    const { senior } = custumers;
+    return calculate(adult, child, senior);
+  } return 0;
 }
 
 module.exports = { calculateEntry, countEntrants };
