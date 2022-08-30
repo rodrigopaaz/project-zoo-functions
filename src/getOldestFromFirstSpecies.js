62ba251a701dedbf5d063199aa784b69c7cdf55b
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-08-29 21:07:04 -0300

    realizado o requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const { species } = data;
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findEmployee = employees.filter((element) =>
+    element.id === id);
+  const firstAnimal = findEmployee.find((element) => element).responsibleFor[0];
+  const animalsById = species.filter((element) => element.id === firstAnimal);
+  const { residents } = animalsById[0];
+  const oldestAnimal = residents.reduce((age1, age2) => (age1.age > age2.age ? age1 : age2));
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
